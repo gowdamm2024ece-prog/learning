f01c9d5bf1b3a6b1ff223be3c25f0ea499ba7426
Gowdam M <gowdam.m2024ece@sece.ac.in>
2026-09-10 09:39:14 +0530

    api/products modified in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.use("/api/products", productRoutes);
+//app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
